e4db371b388fca73ebac06e3b90abcb526e42576
jose.taboada <jose.taboada@digital.hmrc.gov.uk>
2017-08-03 15:47:33 +0100

    Improved output

--- bin/checker.py
+++ bin/checker.py
@@ -1,14 +1,10 @@
 #!/usr/bin/env python
 from checker_lib import *
-import collections
-
 
 args = parseArguments()
 
-print "Workspace {0}".format(args.workspace)
-
-# dev = "{0}/app-config-{1}/{2}.yaml".format( args.workspace, "dev",args.projectName)
-
+if args.verbose:
+    print "Workspace {0}".format(args.workspace)
 
 diff(args, args.envA, args.envB)
 
